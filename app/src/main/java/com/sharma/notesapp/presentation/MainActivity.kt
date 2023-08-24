dcf06da1560e5ca6bfd4740a88941c2813fd32b0
coder481 <hrithiksharma24092000@gmail.com>
2023-08-24 13:04:41 +0530

    Initial Commit

---
+++ app/src/main/java/com/sharma/notesapp/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sharma.notesapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sharma.notesapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
